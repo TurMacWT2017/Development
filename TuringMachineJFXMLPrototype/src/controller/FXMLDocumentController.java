aca195f200ff2bde71abc38d0997900e67594cc3
Nicholas Ahring <nbahring1@buffs.wtamu.edu>
2017-02-02 14:43:05 -0600

    Modification and refactoring to support MVC (Model View Controller) based development moving forward

--- TuringMachineJFXMLPrototype/src/controller/FXMLDocumentController.java
+++ TuringMachineJFXMLPrototype/src/controller/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package turingmachinejfxmlprototype;
+package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 /**
  *
@@ -19,12 +20,16 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Button runButton;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void runButtonClicked(ActionEvent event) {
+        if (runButton.getText().equals("Run")) {
+            runButton.setText("Pause");
+        }
+        else {
+            runButton.setText("Run");
+        }
     }
     
     @Override
