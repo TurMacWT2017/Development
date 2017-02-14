148685e06d250fdc1edec5c6ac1a2eb4acc41462
Nicholas Ahring <nbahring1@buffs.wtamu.edu>
2017-02-14 14:51:58 -0600

    Error checking added for select fields

--- TuringMachineJFXMLPrototype/src/model/Interpreter.java
+++ TuringMachineJFXMLPrototype/src/model/Interpreter.java
@@ -33,6 +33,7 @@ public class Interpreter
         
         StringBuilder sb = new StringBuilder();
         int numberOfLines = 0;
+        int lineNum = 0;
         
         //File file = new File("test.tm");
         try {
@@ -41,6 +42,7 @@ public class Interpreter
             if (in.next().toLowerCase().startsWith("input:")) 
             {
                 initialInput = in.next().replaceAll("input:", "").trim();
+                lineNum++;
             }
             
             while (in.hasNext())
@@ -57,22 +59,27 @@ public class Interpreter
                 // Get the current tape - if left blank, default to tape 1
                 if (i%6 == 0)
                 {
+                    lineNum++;
                     if (tokens[i].equals(""))
                     {
                         tokens[i] = "t1";
-                        System.out.println("\nTape token: " + tokens[i]);
+                        System.out.println("\nTape token: " + tokens[i] + " on line " + lineNum);
                     }
                     else if (tokens[i].equalsIgnoreCase("t1")) 
                     {
-                        System.out.println("\nTape token: " + tokens[i]);
+                        System.out.println("\nTape token: " + tokens[i] + " on line " + lineNum);
                     }
                     else if (tokens[i].equalsIgnoreCase("t2"))
                     {
-                        System.out.println("\nTape 2 token: " + tokens[i]);
+                        System.out.println("\nTape 2 token: " + tokens[i] + " on line " + lineNum);
                     }
                     else if (tokens[i].equalsIgnoreCase("t3"))
                     {
-                        System.out.println("\nTape 3 token: " + tokens[i]);
+                        System.out.println("\nTape 3 token: " + tokens[i] + " on line " + lineNum);
+                    }
+                    else 
+                    {
+                        System.out.println("\nError on line " + lineNum + "Invalid tape");
                     }
                     
                 }
@@ -80,25 +87,25 @@ public class Interpreter
                 // Get the initial state
                 if (i%6 == 1)
                 {
-                    System.out.println("Initial state token: " + tokens[i]);
+                    System.out.println("Initial state token: " + tokens[i] + " on line " + lineNum);
                 }
                 
                 // Get the token being read
                 if (i%6 == 2)
                 {
-                    System.out.println("Read token: " + tokens[i]);
+                    System.out.println("Read token: " + tokens[i] + " on line " + lineNum);
                 }
                 
                 // Write desired token
                 if (i%6 == 3)
                 {
-                    System.out.println("Write token: " + tokens[i]);
+                    System.out.println("Write token: " + tokens[i] + " on line " + lineNum);
                 }
                 
                 // Get the direction the read/write head needs to move
                 if (i%6 == 4)
                 {
-                    System.out.println("Direction token: " + tokens[i]);
+                    System.out.println("Direction token: " + tokens[i] + " on line " + lineNum);
                     if (tokens[i].equalsIgnoreCase("R") || tokens[i].equalsIgnoreCase("right") || tokens[i].equals(">"))
                     {
                         moveRight();
@@ -107,16 +114,20 @@ public class Interpreter
                     {
                         moveLeft();
                     }
-                    else
+                    else if (tokens[i].equalsIgnoreCase("*"))
                     {
                         stay();
+                    }
+                    else 
+                    {
+                        System.out.printf("Invalid direction on line %d", lineNum);
                     }
                 }
                 
                 // Get the end-state of the transition
                 if (i%6 == 5)
                 {
-                    System.out.println("End state token: " + tokens[i]);
+                    System.out.println("End state token: " + tokens[i] + " on line " + lineNum);
                 }
             }
             
